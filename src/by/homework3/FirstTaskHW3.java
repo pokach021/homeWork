7d9319628b72fa9ae5e51c25119a06bd6c727d6b
pokach666 <andrew.pokachailo@yandex.ru>
2020-11-20 23:40:24 +0300

    Добавлена 2я задача

--- src/by/homework3/FirstTaskHW3.java
+++ src/by/homework3/FirstTaskHW3.java
@@ -5,12 +5,16 @@ import java.util.Random;
 public class FirstTaskHW3 {
     public static void main(String[] args) {
         Random random = new Random();
+        String str = "";
         for (int i = 0; i < 100; i++) {
             int numbers1 = random.nextInt(1000);
-            String str = String.valueOf(numbers1);
-            System.out.print(str + " ");
+            str += numbers1 + " ";
+
         }
+        System.out.print(str + " ");
         System.out.println();
+        String regex = "\\b\\d{2}\\b";
+        System.out.println(str.replaceAll(regex, "-1") + " ");
         //StringBuilder
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < 100; i++) {
